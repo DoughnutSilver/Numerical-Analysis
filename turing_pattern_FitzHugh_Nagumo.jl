43b498339036a4aeedb32136ca9478cf609329ad
DoughnutSilver <mao220120222@yahoo.co.jp>
2019-10-27 00:23:25 +0900

    @inboundsで高速化した、(@.,@simdは早くならなかった)

--- turing_pattern_FitzHugh_Nagumo.jl
+++ turing_pattern_FitzHugh_Nagumo.jl
@@ -6,6 +6,9 @@ This program makes turing pattern by using FitzHugh-Nagumo-model
 <FitzHugh-Nagumo-model>
     f(u,v)=u-u^3-v
     g(u,v)=γ(u-αv-β)
+
+<Stability condition>
+    DΔt/Δx^2 <1/2    
 ----------------------------------------------------------------------------------=#
 #using DifferentialEquations
 #using ParameterizedFunctions
@@ -21,9 +24,9 @@ function f(u::Float64,v::Float64)
 end
 
 function g(u::Float64,v::Float64)
-    α = 0.5
-    β = 0.05
-    γ = 26.0
+    α =0.5
+    β =0.0
+    γ =26.0
     return γ*(u-α*v-β)
 end
 
@@ -31,24 +34,22 @@ function diffusion(system_parameter,distribution)
     out=zeros(system_parameter.size,system_parameter.size)
     out=-4*copy(distribution)
     #diff x
-        out+=[distribution[2:system_parameter.size,:]   ; distribution[1,:]']
-        out+=[distribution[system_parameter.size,:]'    ; distribution[1:system_parameter.size-1,:]]
+        @inbounds out+=[distribution[2:system_parameter.size,:]   ; distribution[1,:]']
+        @inbounds out+=[distribution[system_parameter.size,:]'    ; distribution[1:system_parameter.size-1,:]]
     #diff y
-        out+=[distribution[:,2:system_parameter.size]   distribution[:,1]]
-        out+=[distribution[:,system_parameter.size]     distribution[:,1:system_parameter.size-1]]
+        @inbounds out+=[distribution[:,2:system_parameter.size]   distribution[:,1]]
+        @inbounds out+=[distribution[:,system_parameter.size]     distribution[:,1:system_parameter.size-1]]
     local   invdx=1.0/system_parameter.dx^2
     return  out*invdx
 end
 
 function onestep_u(model_parameter,system_parameter,variable)
-    local invdx=1/system_parameter.dx^2
-    variable.u+=system_parameter.dt*(invdx*model_parameter.Du*diffusion(system_parameter,variable.u)+f.(variable.u,variable.v))
+    @inbounds variable.u+=system_parameter.dt*(model_parameter.Du*diffusion(system_parameter,variable.u)+f.(variable.u,variable.v))
     return variable.u
 end
 
 function onestep_v(model_parameter,system_parameter,variable)
-    local invdx=1/system_parameter.dx^2
-    variable.v+=system_parameter.dt*(invdx*model_parameter.Dv*diffusion(system_parameter,variable.v)+g.(variable.u,variable.v))
+    @inbounds variable.v+=system_parameter.dt*(model_parameter.Dv*diffusion(system_parameter,variable.v)+g.(variable.u,variable.v))
     return variable.v
 end
 
@@ -97,12 +98,12 @@ end
 
 mutable struct variable
     u   :: Array{Float64,2}
-    v  :: Array{Float64,2}
+    v   :: Array{Float64,2}
 end
 
 
 model=model_parameter(1.0*10^(-4) ,1.0*10^(-2))
-sys=system_parameter(128,10,75,0.50,0.04) #dx^2>dt
+sys=system_parameter(128,10,75,10^(-2),10^(-4)) #dx^2>dt  #0.50,0.04がよかった
 u=rand(sys.size,sys.size)
 v=rand(sys.size,sys.size)
 var=variable(u,v)
@@ -117,11 +118,14 @@ y_range=1:1:sys.size
 #plt=heatmap(x_range,y_range,var.u)
 #display(plt)
 
-for time in 1:1000
-    global var.u=onestep_u(model,sys,var)
-    global var.v=onestep_v(model,sys,var)
+function calc(model_parameter,system_parameter,variable)
+    for time in 1:1000000
+        global variable.u=onestep_u(model_parameter,system_parameter,variable)
+        global variable.v=onestep_v(model_parameter,system_parameter,variable)
+    end
 end
-heatmap(x_range,y_range,var.u-var.v,size=(400,400))
+
+@timev calc(model,sys,var)
 
 #make_gif(x_range,y_range,model,sys,var)
 
